d498481dd8fecac315a94e644d275d97e31076d1
plastfw <ibanaski24@gmail.com>
2025-05-23 19:16:28 +0300

    add_remoteConfigLogic

--- Assets/Source/Scripts/Analytics/FireBaseInitializer.cs
+++ Assets/Source/Scripts/Analytics/FireBaseInitializer.cs
@@ -1,11 +1,16 @@
-﻿using Firebase;
+﻿using System;
+using System.Threading.Tasks;
+using Firebase;
 using Firebase.Analytics;
+using Firebase.Extensions;
+using Firebase.RemoteConfig;
 using UnityEngine;
 
 namespace Source.Scripts.Analytics
 {
     public class FireBaseInitializer : MonoBehaviour
     {
+        private int _test = 0;
         public bool IsInitialized { get; private set; } = false;
 
         private async void Start()
@@ -17,10 +22,57 @@ namespace Source.Scripts.Analytics
             {
                 FirebaseAnalytics.SetAnalyticsCollectionEnabled(true);
                 IsInitialized = true;
-                Debug.Log("Firebase Analytics initialized.");
+                FetchDataAsync();
+                Debug.Log("Firebase initialized.");
             }
             else
                 Debug.LogError("Could not resolve Firebase dependencies: " + task.Result);
+
+            // await FetchDataAsync().ContinueWith(FetchComplete);
+            // FetchComplete();
+        }
+
+        // private UniTask FetchDataAsync()
+        // {
+        //     return FirebaseRemoteConfig.DefaultInstance
+        //         .FetchAsync(TimeSpan.Zero)
+        //         .AsUniTask();
+        // }
+
+        public Task FetchDataAsync()
+        {
+            Task fetchTask = FirebaseRemoteConfig.DefaultInstance
+                .FetchAsync(TimeSpan.Zero);
+
+            return fetchTask.ContinueWithOnMainThread(FetchComplete);
+        }
+
+        private void FetchComplete(Task task)
+        {
+            if (!task.IsCompleted)
+            {
+                Debug.LogError("Retrieval hasn't finished");
+                return;
+            }
+
+            var remoteConfig = FirebaseRemoteConfig.DefaultInstance;
+            var info = remoteConfig.Info;
+
+            if (info.LastFetchStatus != LastFetchStatus.Success)
+            {
+                Debug.LogError(
+                    $"{nameof(FetchComplete)} was unsuccessful \n {nameof(info.LastFetchStatus)}: {info.LastFetchStatus}");
+                return;
+            }
+
+            remoteConfig.ActivateAsync()
+                .ContinueWithOnMainThread(task =>
+                {
+                    Debug.Log("Remote data load and ready to use");
+                    var value = (int)FirebaseRemoteConfig.DefaultInstance.GetValue("testValue").LongValue;
+                    _test = value;
+                    Debug.LogWarning(_test);
+                });
         }
     }
 }
